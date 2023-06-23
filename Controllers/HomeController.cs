2b73d8b3db9bb2737694f8d4ac594b290d4275e0
Victor Ajibade <adesojimav@gmail.com>
2023-06-23 09:31:32 +0100

    updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace MavHotel.Controllers
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MavHotel.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
